88bf0538539f29aa0f2e077b6f52fa0b72ad5d28
hao <hao.aiavn@gmail.com>
2021-04-22 15:51:40 +0700

    POST METHOD

--- src/app.js
+++ src/app.js
@@ -9,7 +9,13 @@ const port = 3000;
 // static file
 app.use(express.static(path.join(__dirname, 'public')))
 // show log res, req when F5
-app.use(morgan("combined"));
+//app.use(morgan("combined"));
+
+//middleware
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 // change .extname
 app.engine('hbs', exphbs({
     extname: ".hbs"
@@ -21,5 +27,9 @@ app.get('/', (req, res)=> res.render('home'));
 app.get('/about', (req, res)=> res.render('about'));
 app.get('/news', (req, res)=> res.render('news'));
 app.get('/search', (req, res)=> res.render('search'));
+app.post('/search', (req, res)=> {
+    res.render('search');
+    console.log(req.body);
+});
 
 app.listen(port, ()=> console.log(`Listenning at localhost:${port}`));
